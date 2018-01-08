80a6df097d4626fdcf3b0299b24d50d7f236f773
Bartosz Jaśko <bjasko97@gmail.com>
2018-01-08 22:02:22 +0100

    Added directories

---
+++ include/Factory.h
@@ -0,0 +1,16 @@
+//
+// Created by bartek on 1/8/18.
+//
+
+#ifndef PRODUCTIONLINE_SIM_FACTORY_H
+#define PRODUCTIONLINE_SIM_FACTORY_H
+
+
+#include <vector>
+
+class Factory {
+
+};
+
+
+#endif //PRODUCTIONLINE_SIM_FACTORY_H

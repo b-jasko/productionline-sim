5f012f9ea47eabc1d841d622b26ece1911ca8fa2
Bartosz Jaśko <bjasko97@gmail.com>
2018-01-13 23:14:30 +0100

    Added some Factory methods

--- src/Factory.cpp
+++ src/Factory.cpp
@@ -15,3 +15,31 @@ const std::list<Worker> &Factory::getWorkers() const {
 const std::list<Storehouse> &Factory::getStorehouses() const {
     return storehouses;
 }
+
+void Factory::addRamp(Ramp _ramp) {
+    ramps.push_back(_ramp);
+}
+
+void Factory::addWorker(Worker _worker) {
+    workers.push_back(_worker);
+}
+
+void Factory::addStorehouse(Storehouse _storehouse) {
+    storehouses.push_back(_storehouse);
+}
+
+void Factory::removeRamp(ElementID) {
+
+}
+
+void Factory::removeWorker(ElementID) {
+
+}
+
+void Factory::removeStorehouse(ElementID) {
+
+}
+
+bool Factory::isConsistent() {
+
+}
